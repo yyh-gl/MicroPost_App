cc74797c90fa2c54bb80914f4fbcf0bce10b1d57
yu_honda <yhonda@mikilab.doshisha.ac.jp>
2017-08-21 03:16:51 +0900

    ログイン情報を一時的にクッキーに保存する機能を実装

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,17 @@
+# coding: utf-8
 module SessionsHelper
+  # 渡されたユーザーでログインする
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  # ログイン中のユーザ名（いるなら）を返す
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  # ユーザーがログインしていればtrue、その他ならfalseを返す
+  def logged_in?
+    !current_user.nil?
+  end
 end

d04c9755a90556e8746d4071d3d0a047b59b8e4d
yu_honda <yhonda@mikilab.doshisha.ac.jp>
2017-08-21 01:20:22 +0900

    ログインページの実装と間違った情報をフラッシュで通知する機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/show'
-
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
@@ -8,5 +6,8 @@ Rails.application.routes.draw do
   get  '/news',    to: 'static_pages#news'
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :users
 end
